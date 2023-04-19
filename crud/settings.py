16a2a0ad33cb0b9a3111605772992ce91022aae9
syyoung <yuyoung.dev@gmail.com>
2023-04-19 14:49:33 +0900

    write

--- crud/settings.py
+++ crud/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['port-0-cloudtype-1maxx2algn95mv3.sel3.cloudtype.app']
 
 
 # Application definition
